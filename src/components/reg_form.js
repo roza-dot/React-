11be1baf257c8c1b4daaee53d6f8af03d86bb764
Roza Grigorkiv <roza_grigorkiv@yahoo.com>
2020-05-15 10:11:00 -0600

    first commit

--- src/components/reg_form.js
+++ src/components/reg_form.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
+
+
 const RegisterForm = props => {
-    const [ formState, setFormState ] = useState({
+    const [formState, setFormState] = useState({
         firstName: "",
         lastName: "",
         email: "",
@@ -13,48 +15,48 @@ const RegisterForm = props => {
             [event.target.name]: event.target.value
         });
     }
-    const onSubmitHandler = event => {
-        event.preventDefault();
-        console.log(formState);
-    }
+    const onSubmitHandler = event => {
+        event.preventDefault();
+        console.log(formState);
+    }
     // const validate = (email) => {
     //     const expression = ^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$
     // }
-   
-    return(
+
+    return (
         <div>
             <form onSubmit={onSubmitHandler}>
-                <div class= "box">
+                <div class="box">
                     <label>First Name</label>
-                    <input type="text" name="firstName" onChange={onChangeHandler}/>
+                    <input type="text" name="firstName" onChange={onChangeHandler} />
                     {formState.firstName.length > 0 && formState.firstName.length < 5 && (<span>Name not long enough</span>)}
-                    <br/> 
-                </div> 
-                <div class="box">     
+                    <br />
+                </div>
+                <div class="box">
                     <label>Last Name</label>
-                    <input type="text" name="lastName" onChange={onChangeHandler}/>
+                    <input type="text" name="lastName" onChange={onChangeHandler} />
                     {formState.lastName.length > 0 && formState.lastName.length < 5 && (<span>Not long enough</span>)}
-                    <br/> 
-                </div>  
-                <div class="box">  
+                    <br />
+                </div>
+                <div class="box">
                     <label>Email</label>
-                    <input type="email" name="email" onChange={onChangeHandler}/>
-                    {formState.email.length > 2 && (<span>email must be a least 2 characters</span>) }
-                    <br/>  
-                </div> 
-                <div class="box">   
+                    <input type="email" name="email" onChange={onChangeHandler} />
+                    {formState.email.length > 2 && (<span>email must be a least 2 characters</span>)}
+                    <br />
+                </div>
+                <div class="box">
                     <label>Password</label>
-                    <input type="password" name="password" onChange={onChangeHandler}/>
+                    <input type="password" name="password" onChange={onChangeHandler} />
                     {formState.password.length > 0 && formState.password.length < 8 && (<span>password not long enough</span>)}
-                    <br/> 
-                </div> 
-                <div class="box">     
+                    <br />
+                </div>
+                <div class="box">
                     <label>Confirm Password</label>
-                    <input type="password" name="confirmPassword" onChange={onChangeHandler}/>
+                    <input type="password" name="confirmPassword" onChange={onChangeHandler} />
                     {formState.password !== formState.confirmPassword && (<span>Password must match</span>)}
-                    <br/>
-                </div>   
-                <input class= "submit_button" type="submit" />     
+                    <br />
+                </div>
+                <input class="submit_button" type="submit" />
             </form>
             <div class="text">
                 <p>First Name:{formState.firstName}</p>
